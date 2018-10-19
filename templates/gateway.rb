4c15bb4300b5a2a48c3c38d2270d0e4e53e04d45
Faissal Bensefia <faissal@madetech.com>
2018-10-19 14:04:04 +0100

    Fix gateway template

--- templates/gateway.rb
+++ templates/gateway.rb
@@ -6,9 +6,9 @@ module Template
   end
 
   def self.main(*args)
-    @domain_class_name = args[0]
+    @gateway_class_name = args[0]
 
-    Templates.create('gateway', "src/Domain/#{@gateway_class_name}", binding)
+    Templates.create('gateway', "src/Gateway/#{@gateway_class_name}", binding)
     print "Gateway '#{@gateway_class_name}' created!\n"
   end
 end
